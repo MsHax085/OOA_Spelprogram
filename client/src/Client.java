fe7af51771b59a4910a1cb358a1892f211e812e0
Richard Dahlgren <richard@richarddahlgren.se>
2016-01-25 14:30:09 +0100

    Startup
    Basic structure

---
+++ client/src/Client.java
@@ -0,0 +1,12 @@
+package src;
+
+/**
+ *
+ * @author Richard
+ */
+public class Client {
+    
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD!");
+    }
+}
---
+++ server/src/Server.java
@@ -0,0 +1,12 @@
+package src;
+
+/**
+ *
+ * @author Richard
+ */
+public class Server {
+    
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD!");
+    }
+}
