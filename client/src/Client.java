9dd43e93d2e3a321a893961c4f76a82a764776ff
Richard Dahlgren <richard@richarddahlgren.se>
2016-01-27 14:42:00 +0100

    Revert everything

--- client/src/Client.java
+++ client/src/Client.java
@@ -2,20 +2,11 @@ package src;
 
 /**
  *
- * @author Richard och lite oscar
+ * @author Richard
  */
 public class Client {
 	
     public static void main(String[] args) {
         System.out.println("HELLO WORLD!");
-		
-		if(isOscarBest())
-			System.out.println("Oscar is nice gaj");
-		else
-			System.out.println("Richard is crayey, ^-(*.*)-^");
-    }
-    
-    private static boolean isOscarBest(){
-    	return false;
     }
 }
