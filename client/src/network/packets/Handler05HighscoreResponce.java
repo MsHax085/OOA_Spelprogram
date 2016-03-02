612bbfa4aca3a08fbce6c16d45765b433538c6d8
Fontn <erik9115@gmail.com>
2016-03-02 19:58:41 +0100

    Highscore på client ränsas innan den nya sparas

--- client/src/network/packets/Handler05HighscoreResponce.java
+++ client/src/network/packets/Handler05HighscoreResponce.java
@@ -2,6 +2,7 @@ package src.network.packets;
 
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,6 +26,13 @@ public class Handler05HighscoreResponce implements ImplPacketHandler {
         try {
             // Opcode (first short) already read
             int numberOfHighscores = dis.readInt();
+            
+            //Clears the local highscorelist
+            final Iterator itr = Database.getInstance().getHighscore();
+            while (itr.hasNext()) {
+        	itr.next();
+            	itr.remove();
+            }
             
             // Adds the new list.
             for (int i = 0; i < numberOfHighscores; i++) {
